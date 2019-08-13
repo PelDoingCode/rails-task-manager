aeb3abcf3997e7fa52028e983e0452f4f3370ffe
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-08-13 13:30:25 +0100

    add the create task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,7 +13,9 @@ def new
 end
 
 def create
-
+  task = Task.new(task_params)
+  task.save
+  redirect_to tasks_path
 end
 
 def edit
@@ -27,6 +29,13 @@ def delete
 end
 
 def destroy
+end
+
+private
+# protection from attack !!!
+
+def task_params
+  params.require(:task).permit(:title, :details, :completed)
 end
 
 
