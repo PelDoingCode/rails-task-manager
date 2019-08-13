cc0e6a8d82b26053bbc398123251d4f7a631ffe1
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-08-13 12:18:56 +0100

    first things done to display tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
 
 def show
+  @tasks = Task.find(params[:id])
 end
 
 def index
@@ -12,6 +13,7 @@ def new
 end
 
 def create
+
 end
 
 def edit
@@ -29,55 +31,3 @@ end
 
 
 end
-
-
-
-
-
-
-
-
-
-# class TasksController < ApplicationController
-#   before_action :set_task, only: [ :show, :edit, :update, :destroy ]
-
-#   def index
-#     @tasks = Task.all
-#   end
-
-#   def show
-#   end
-
-#   def new
-#     @task = Task.new
-#   end
-
-#   def create
-#     task = Task.new(task_params)
-#     task.save
-#     redirect_to tasks_path
-#   end
-
-#   def edit
-#   end
-
-#   def update
-#     @task.update(task_params)
-#     redirect_to task_path(@task)
-#   end
-
-#   def destroy
-#     @task.destroy
-#     redirect_to tasks_path
-#   end
-
-#   private
-
-#   def set_task
-#     @task = Task.find(params[:id])
-#   end
-
-#   def task_params
-#     params.require(:task).permit(:title, :details, :completed)
-#   end
-# end
